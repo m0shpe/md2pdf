2b67a80466049cf4949f292e6b2741ce7f1e2fdd
moritz <m.schoppe2105@gmail.com>
2022-10-06 12:03:15 +0200

    add convert to md

--- md2pdf.py
+++ md2pdf.py
@@ -1,8 +1,18 @@
 import os
 import aspose.words as aw
+import sys
 
 md_files = {}
 directory = str(input("Enter path: "))
+mode = str(input("Convert to (pdf/md): "))
+if(mode == "pdf"):
+    search_extension = ".md"
+    mode_extension = ".pdf"
+elif(mode == "md"):
+    search_extension = ".pdf"
+    mode_extension = ".md"
+else:
+    sys.exit("Wrong mode: " + mode)
 result_directory = "created_pdf_files"
 result_path = directory + "\\" + result_directory 
 
@@ -13,7 +23,7 @@ for root, dirs, files in os.walk(directory, topdown=True):
     for file in files:
         path_to_file = os.path.join(root, file)
         filename, file_extension = os.path.splitext(path_to_file)
-        if file_extension == ".md":
+        if file_extension == search_extension:
             md_files[file] = root.split(directory + "\\")[1]
 
 print()
@@ -29,5 +39,5 @@ for filename in md_files:
         os.makedirs(file_abs_path)
     name, extension = os.path.splitext(filename)
     doc = aw.Document(file_rel_path + "\\" + filename)
-    doc.save(file_abs_path + "\\" + name + ".pdf")
+    doc.save(file_abs_path + "\\" + name + mode_extension)
 print("Finished...")
